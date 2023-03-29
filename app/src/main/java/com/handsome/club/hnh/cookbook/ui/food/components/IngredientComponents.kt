13be3a057b8b1f2f20d33064010d6c0077f486db
darx <krzysztofwycislo@gmail.com>
2023-03-29 23:34:31 +0200

    Ingredients new concept

--- app/src/main/java/com/handsome/club/hnh/cookbook/ui/food/components/IngredientComponents.kt
+++ app/src/main/java/com/handsome/club/hnh/cookbook/ui/food/components/IngredientComponents.kt
@@ -1,30 +1,52 @@
 package com.handsome.club.hnh.cookbook.ui.food.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import com.handsome.club.hnh.cookbook.model.food.Ingredient
-import com.handsome.club.hnh.cookbook.ui.theme.VertSpacerM
+import com.handsome.club.hnh.cookbook.ui.theme.HorSpacerXS
 import com.handsome.club.hnh.cookbook.ui.theme.VertSpacerXS
 
 
 @Composable
 fun IngredientsList(ingredients: List<Ingredient>) {
-    Column(Modifier.fillMaxWidth()) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
 
-        ingredients.forEach {
-            Row {
-                Text(text = "${it.percentage}%")
+        ingredients.sortedBy { -it.percentage }
+            .forEach {
 
                 VertSpacerXS()
 
-                Text(text = it.name)
-            }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(4.dp)
+                            .clip(CircleShape)
+                            .background(MaterialTheme.colorScheme.surfaceVariant)
+                    )
 
-            VertSpacerM()
-        }
+                    HorSpacerXS()
+
+                    Text(text = "${it.name}:")
+
+                    HorSpacerXS()
+
+                    Text(text = "${it.percentage}%")
+                }
+
+                VertSpacerXS()
+
+            }
     }
 }
